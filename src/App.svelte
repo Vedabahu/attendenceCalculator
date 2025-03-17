ffa8f78dab906c398799f125c02637b72a684fa3
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2025-03-17 23:44:33 +0530

    Added logo and by line

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,16 @@
   import NonIiit from "./lib/nonIIIT.svelte";
     import IiitDwd from "./lib/IIITDwd.svelte";
 
-  let forIIIT = $state("IIIT Dharwad");
+  let forIIIT = $state("Normal");
 </script>
 
 <div class="p-4 min-w-96 mt-7 sm:w-xl sm:mx-auto sm:mt-10">
-  <h1 class="text-4xl pb-10 flex text-center justify-center font-bold">
-    Attendance Calculator
+
+  <h1 class="text-4xl pb-10 flex-col text-center justify-center font-bold h-fit">
+    Attendance Calculator <br />
+    <span class="text-sm">By Vedabahu</span>
   </h1>
+  
   <Tabs
     value={forIIIT}
     onValueChange={(e) => {
